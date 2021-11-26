9a83b64295be1c761db0d811167a333cb24e0d97
Jenna Nguyen <jenna.jtn@gmail.com>
2021-11-26 09:11:54 -0500

    demo day

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb+srv://devjen:rcdemo@cluster0.ewfjb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    'dbName': 'demoday'
-};
